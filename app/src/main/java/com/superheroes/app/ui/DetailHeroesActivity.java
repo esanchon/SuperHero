d6206059dea56bf4e55691a25ad065732127ae14
esanchon <esanchon@gmail.com>
2023-02-15 13:06:35 +0100

    menu compartir heroe
    creado menú

--- app/src/main/java/com/superheroes/app/ui/DetailHeroesActivity.java
+++ app/src/main/java/com/superheroes/app/ui/DetailHeroesActivity.java
@@ -4,11 +4,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
@@ -18,12 +21,15 @@ import com.superheroes.app.domain.models.MarvelHero;
 
 import org.w3c.dom.Text;
 
-public class DetailHeroesActivity extends AppCompatActivity{
+public class DetailHeroesActivity extends AppCompatActivity {
 
     private ScrollView mscrollView;
     private ImageView mImageView;
 
     private MarvelHero currentHeroSelected;
+
+    Intent intent;
+    String detalleHeroe;
 
     public static void start(Context context, MarvelHero marvelHero) {
         Intent intent = new Intent(context, DetailHeroesActivity.class);
@@ -36,8 +42,8 @@ public class DetailHeroesActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if(savedInstanceState != null &&savedInstanceState.getParcelable("hero") != null){
-           currentHeroSelected = savedInstanceState.getParcelable("hero");
+        if (savedInstanceState != null && savedInstanceState.getParcelable("hero") != null) {
+            currentHeroSelected = savedInstanceState.getParcelable("hero");
         } else {
             Bundle extras = getIntent().getExtras();
             currentHeroSelected = extras.getParcelable("hero");
@@ -54,17 +60,16 @@ public class DetailHeroesActivity extends AppCompatActivity{
         TextView intelligence = findViewById(R.id.tv_hero_intelligence);
         TextView group = findViewById(R.id.tv_hero_group);
 
-        name.setText(currentHeroSelected.getName());
-        realName.setText(currentHeroSelected.getRealName());
-        gender.setText(currentHeroSelected.getGender());
-        power.setText(currentHeroSelected.getPower());
-        intelligence.setText(currentHeroSelected.getIntelligence());
-        group.setText(currentHeroSelected.getGroups());
+        name.setText(getString(R.string.hero_detail_name) + currentHeroSelected.getName());
+        realName.setText(getString(R.string.hero_detail_realname) + currentHeroSelected.getRealName());
+        gender.setText(getString(R.string.hero_detail_gender) + currentHeroSelected.getGender());
+        power.setText(getString(R.string.hero_detail_power) + currentHeroSelected.getPower());
+        intelligence.setText(getString(R.string.hero_detail_intelligence) + currentHeroSelected.getIntelligence());
+        group.setText(getString(R.string.hero_detail_groups) + currentHeroSelected.getGroups());
 
         Glide.with(this).load(currentHeroSelected.getPhoto()).into(mImageView);
 
         getSupportActionBar().setTitle(getString(R.string.detail));
-
 
 
     }
@@ -75,4 +80,33 @@ public class DetailHeroesActivity extends AppCompatActivity{
         super.onSaveInstanceState(savedInstanceState);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.top_app_bar_detailhero, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.share_content:
+                //TODO create presenter
+                //presenter.onMenuShareClicked();
+                intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                detalleHeroe = getResources().getString(R.string.hero_detail_name) + currentHeroSelected.getName()
+                                +"\n" + getResources().getString(R.string.hero_detail_power) + currentHeroSelected.getPower();
+                intent.putExtra(Intent.EXTRA_TEXT, detalleHeroe);
+                Intent shareIntent = Intent.createChooser(intent, "Compartir héroe");
+                startActivity(shareIntent);
+
+                break;
+
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
+
